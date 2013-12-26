008ebf377b9812909657c1b8bedb306a8cc2de7c
wikey2013 <919404881@qq.com>
2013-12-27 00:07:57 +0800

    添加新功能菜单
    添加新功能菜单

--- PigManager/PigManagerSystem/PigManagerSystem/MDIParent1.Designer.cs
+++ PigManager/PigManagerSystem/PigManagerSystem/MDIParent1.Designer.cs
@@ -43,6 +43,8 @@
             this.tileHorizontalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.closeAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.百度查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolUserName = new System.Windows.Forms.ToolStripStatusLabel();
             this.UserNameToolStrip = new System.Windows.Forms.ToolStripStatusLabel();
@@ -52,8 +54,12 @@
             this.systemTime = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.百度查询ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.饲料管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.药品管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.日用品管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.商品猪销售ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.客户管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.SuspendLayout();
@@ -72,7 +78,7 @@
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.MdiWindowListItem = this.windowsMenu;
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(878, 25);
+            this.menuStrip.Size = new System.Drawing.Size(878, 24);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "MenuStrip";
             // 
@@ -82,23 +88,30 @@
             this.PigMenu.Image = ((System.Drawing.Image)(resources.GetObject("PigMenu.Image")));
             this.PigMenu.ImageTransparentColor = System.Drawing.SystemColors.ActiveBorder;
             this.PigMenu.Name = "PigMenu";
-            this.PigMenu.Size = new System.Drawing.Size(84, 21);
+            this.PigMenu.Size = new System.Drawing.Size(81, 20);
             this.PigMenu.Text = "种猪管理";
             this.PigMenu.Click += new System.EventHandler(this.PigMenu_Click);
             // 
             // SellMenu
             // 
+            this.SellMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.商品猪销售ToolStripMenuItem,
+            this.客户管理ToolStripMenuItem});
             this.SellMenu.Image = ((System.Drawing.Image)(resources.GetObject("SellMenu.Image")));
             this.SellMenu.Name = "SellMenu";
-            this.SellMenu.Size = new System.Drawing.Size(84, 21);
+            this.SellMenu.Size = new System.Drawing.Size(81, 20);
             this.SellMenu.Text = "销售管理";
             this.SellMenu.Click += new System.EventHandler(this.SellMenu_Click);
             // 
             // KucunMenu
             // 
+            this.KucunMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.饲料管理ToolStripMenuItem,
+            this.药品管理ToolStripMenuItem,
+            this.日用品管理ToolStripMenuItem});
             this.KucunMenu.Image = ((System.Drawing.Image)(resources.GetObject("KucunMenu.Image")));
             this.KucunMenu.Name = "KucunMenu";
-            this.KucunMenu.Size = new System.Drawing.Size(84, 21);
+            this.KucunMenu.Size = new System.Drawing.Size(81, 20);
             this.KucunMenu.Text = "库存管理";
             this.KucunMenu.Click += new System.EventHandler(this.KucunMenu_Click);
             // 
@@ -106,7 +119,7 @@
             // 
             this.MoneyMenu.Image = ((System.Drawing.Image)(resources.GetObject("MoneyMenu.Image")));
             this.MoneyMenu.Name = "MoneyMenu";
-            this.MoneyMenu.Size = new System.Drawing.Size(84, 21);
+            this.MoneyMenu.Size = new System.Drawing.Size(81, 20);
             this.MoneyMenu.Text = "财务管理";
             this.MoneyMenu.Click += new System.EventHandler(this.MoneyMenu_Click);
             // 
@@ -114,7 +127,7 @@
             // 
             this.MemberMenu.Image = ((System.Drawing.Image)(resources.GetObject("MemberMenu.Image")));
             this.MemberMenu.Name = "MemberMenu";
-            this.MemberMenu.Size = new System.Drawing.Size(84, 21);
+            this.MemberMenu.Size = new System.Drawing.Size(81, 20);
             this.MemberMenu.Text = "人员管理";
             this.MemberMenu.Click += new System.EventHandler(this.MemberMenu_Click);
             // 
@@ -122,7 +135,7 @@
             // 
             this.SystemSetingMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("SystemSetingMenuItem.Image")));
             this.SystemSetingMenuItem.Name = "SystemSetingMenuItem";
-            this.SystemSetingMenuItem.Size = new System.Drawing.Size(84, 21);
+            this.SystemSetingMenuItem.Size = new System.Drawing.Size(81, 20);
             this.SystemSetingMenuItem.Text = "系统管理";
             this.SystemSetingMenuItem.Click += new System.EventHandler(this.SystemSetingMenuItem_Click);
             // 
@@ -135,47 +148,62 @@
             this.closeAllToolStripMenuItem});
             this.windowsMenu.Image = ((System.Drawing.Image)(resources.GetObject("windowsMenu.Image")));
             this.windowsMenu.Name = "windowsMenu";
-            this.windowsMenu.Size = new System.Drawing.Size(84, 21);
+            this.windowsMenu.Size = new System.Drawing.Size(81, 20);
             this.windowsMenu.Text = "窗口控制";
             // 
             // cascadeToolStripMenuItem
             // 
             this.cascadeToolStripMenuItem.Name = "cascadeToolStripMenuItem";
-            this.cascadeToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.cascadeToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.cascadeToolStripMenuItem.Text = "层叠(&C)";
             this.cascadeToolStripMenuItem.Click += new System.EventHandler(this.CascadeToolStripMenuItem_Click);
             // 
             // tileVerticalToolStripMenuItem
             // 
             this.tileVerticalToolStripMenuItem.Name = "tileVerticalToolStripMenuItem";
-            this.tileVerticalToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.tileVerticalToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.tileVerticalToolStripMenuItem.Text = "垂直平铺(&V)";
             this.tileVerticalToolStripMenuItem.Click += new System.EventHandler(this.TileVerticalToolStripMenuItem_Click);
             // 
             // tileHorizontalToolStripMenuItem
             // 
             this.tileHorizontalToolStripMenuItem.Name = "tileHorizontalToolStripMenuItem";
-            this.tileHorizontalToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.tileHorizontalToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.tileHorizontalToolStripMenuItem.Text = "水平平铺(&H)";
             this.tileHorizontalToolStripMenuItem.Click += new System.EventHandler(this.TileHorizontalToolStripMenuItem_Click);
             // 
             // closeAllToolStripMenuItem
             // 
             this.closeAllToolStripMenuItem.Name = "closeAllToolStripMenuItem";
-            this.closeAllToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.closeAllToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
             this.closeAllToolStripMenuItem.Text = "全部关闭(&L)";
             this.closeAllToolStripMenuItem.Click += new System.EventHandler(this.CloseAllToolStripMenuItem_Click);
             // 
             // helpMenu
             // 
             this.helpMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
             this.百度查询ToolStripMenuItem,
             this.关于ToolStripMenuItem});
             this.helpMenu.Image = ((System.Drawing.Image)(resources.GetObject("helpMenu.Image")));
             this.helpMenu.Name = "helpMenu";
-            this.helpMenu.Size = new System.Drawing.Size(77, 21);
+            this.helpMenu.Size = new System.Drawing.Size(75, 20);
             this.helpMenu.Text = "帮助(&H)";
             this.helpMenu.Click += new System.EventHandler(this.helpMenu_Click);
+            // 
+            // 百度查询ToolStripMenuItem
+            // 
+            this.百度查询ToolStripMenuItem.Name = "百度查询ToolStripMenuItem";
+            this.百度查询ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.百度查询ToolStripMenuItem.Text = "百度查询";
+            this.百度查询ToolStripMenuItem.Click += new System.EventHandler(this.百度查询ToolStripMenuItem_Click);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
             // 
             // statusStrip
             // 
@@ -212,34 +240,56 @@
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(528, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(773, 12);
             this.toolStripStatusLabel1.Text = "1.01                                                                             " +
-                "                                               ";
+    "                                               ";
             // 
             // toolStripStatusLabel3
             // 
             this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
-            this.toolStripStatusLabel3.Size = new System.Drawing.Size(59, 17);
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(59, 12);
             this.toolStripStatusLabel3.Text = "系统时间:";
             // 
             // systemTime
             // 
             this.systemTime.Name = "systemTime";
-            this.systemTime.Size = new System.Drawing.Size(0, 17);
+            this.systemTime.Size = new System.Drawing.Size(0, 0);
             // 
-            // 百度查询ToolStripMenuItem
+            // 饲料管理ToolStripMenuItem
             // 
-            this.百度查询ToolStripMenuItem.Name = "百度查询ToolStripMenuItem";
-            this.百度查询ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.百度查询ToolStripMenuItem.Text = "百度查询";
-            this.百度查询ToolStripMenuItem.Click += new System.EventHandler(this.百度查询ToolStripMenuItem_Click);
+            this.饲料管理ToolStripMenuItem.Name = "饲料管理ToolStripMenuItem";
+            this.饲料管理ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.饲料管理ToolStripMenuItem.Text = "饲料管理";
             // 
-            // 关于ToolStripMenuItem
+            // 药品管理ToolStripMenuItem
             // 
-            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
-            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.关于ToolStripMenuItem.Text = "关于";
-            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            this.药品管理ToolStripMenuItem.Name = "药品管理ToolStripMenuItem";
+            this.药品管理ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.药品管理ToolStripMenuItem.Text = "药品管理";
+            // 
+            // 日用品管理ToolStripMenuItem
+            // 
+            this.日用品管理ToolStripMenuItem.Name = "日用品管理ToolStripMenuItem";
+            this.日用品管理ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.日用品管理ToolStripMenuItem.Text = "日用品管理";
+            // 
+            // 商品猪销售ToolStripMenuItem
+            // 
+            this.商品猪销售ToolStripMenuItem.Name = "商品猪销售ToolStripMenuItem";
+            this.商品猪销售ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.商品猪销售ToolStripMenuItem.Text = "商品猪销售";
+            // 
+            // 客户管理ToolStripMenuItem
+            // 
+            this.客户管理ToolStripMenuItem.Name = "客户管理ToolStripMenuItem";
+            this.客户管理ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.客户管理ToolStripMenuItem.Text = "客户管理";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem1.Text = "计算器";
             // 
             // MDIParent1
             // 
@@ -290,6 +340,12 @@
         private System.Windows.Forms.ToolStripMenuItem MoneyMenu;
         private System.Windows.Forms.ToolStripMenuItem 百度查询ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 商品猪销售ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 客户管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 饲料管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 药品管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 日用品管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
     }
 }
 
